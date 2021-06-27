fe92b3a24dedf231a28b1abb2ed7ff24d127e2b3
chabo1132 <77046637+chabo1132@users.noreply.github.com>
2021-06-27 17:46:16 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 'use strict';
-{
+
   function update(sum) {
     document.querySelector('input').value = sum;
   }
   function append(sum) {
     document.querySelector('input').value += sum;
   }
-}
+  function calc() 
+  {
+    const v = document.querySelector('input').value;
+    try {
+    const f = new Function('return ' + v )
+    update( f().toString() );
+    }catch(error) {
+    update(error);
+    }
+  }
